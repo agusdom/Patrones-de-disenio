8d0d9a4d77247ba1e43349fcd11f45dfd5b8968b
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-02 18:39:58 -0300

    Add files via upload

---
+++ Singleton/Singleton/Singleton/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciación por constructor prohíbido por ser "privado"
+            //Conexion c = new Conexion();
+            Conexion c = Conexion.getInstancia();
+            c.conectar();
+            c.desconectar();
+
+            bool rspt = c is Conexion;
+            Console.WriteLine(rspt);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Conexion
+    {
+        //Declacion
+        private static Conexion instancia;
+        //private static Conexion instancia = new Conexion();
+        //Para evitar instancia mediante operador "new"
+        private Conexion() { 
+        
+        }
+        /*
+         * Para obtener la instancia unicamente por este metodo.
+         * Notase la palabra reservada "static" hace posible el acceso mediante Clase.metodo
+         */
+        public static Conexion getInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new Conexion(); 
+            }
+            return instancia;
+        }
+        //Metodo prueba
+        public void conectar() {
+            Console.WriteLine("Me conecte a la BD");
+        }
+        //Metodo prueba
+        public void desconectar()
+        {
+            Console.WriteLine("Me desconecte a la BD");
+        }
+    }
+}
