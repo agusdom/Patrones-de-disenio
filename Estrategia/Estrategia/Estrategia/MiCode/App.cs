4b716074a962a4c0f01ee95a33fb66f2ac43601f
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-03 21:38:28 -0300

    Add files via upload

---
+++ Estrategia/Estrategia/Estrategia/MiCode/App.cs
@@ -0,0 +1,19 @@
+﻿using Estrategia.Estrategia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrategia
+{
+    internal class App
+    {
+        static void Main(string[] args)
+        {
+            Contexto contexto = new Contexto(new AntivirusAvanzado());
+            contexto.Ejecutar();
+            Console.ReadLine();
+        }
+    }
+}
