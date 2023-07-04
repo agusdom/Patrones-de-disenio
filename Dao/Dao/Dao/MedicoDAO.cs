de8a228200913ca60a1956eb9b45f7282c65fd81
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-03 21:30:59 -0300

    Add files via upload

---
+++ Dao/Dao/Dao/MedicoDAO.cs
@@ -0,0 +1,14 @@
+﻿using Dao.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao.Dao
+{
+    public interface MedicoDAO : CRUD<Medico>
+    {
+        void Curar(Medico medico);
+    }
+}
---
+++ Dao/Dao/Model/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao.Model
+{
+    public class Persona
+    {
+        private int id;
+        private String nombre;
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public String getNombre()
+        {
+            return nombre;
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+
+    }
+}
