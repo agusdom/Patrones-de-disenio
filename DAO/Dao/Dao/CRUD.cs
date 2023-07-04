9a0bfed844725328d90ec58a8eec9517fa87cd67
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-03 21:37:06 -0300

    Add files via upload

---
+++ DAO/Dao/Dao/CRUD.cs
@@ -0,0 +1,18 @@
+﻿using Dao.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao.Dao
+{
+    public interface CRUD<T>
+    {
+        List<T> ListarTodos();
+        T LeerPorId(int id);
+        void Registrar(T t);
+        void Actualizar(T t);
+        void Eliminar(int id);
+    }
+}
---
+++ DAO/Dao/Dao/MedicoDAOImpl.cs
@@ -0,0 +1,49 @@
+﻿using Dao.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao.Dao
+{
+    public class MedicoDAOImpl : MedicoDAO
+    {
+        public List<Medico> ListarTodos()
+        {
+            List<Medico> lista = new List<Medico>();
+            Medico doc = new Medico();
+            doc.setId(1);
+            doc.setNombre("Agustin Dominguez");
+
+            lista.Add(doc);
+
+            return lista;
+        }
+
+        public void Registrar(Medico medico)
+        {
+            Console.WriteLine("Medico " + medico.getNombre() + " registrado.");
+        }
+
+        public void Actualizar(Medico medico)
+        {
+            Console.WriteLine("Medico " + medico.getNombre() + " actualizado.");
+        }
+
+        public void Eliminar(int id)
+        {
+            Console.WriteLine("Id:" + id + " eliminado.");
+        }
+
+        public Medico LeerPorId(int id)
+        {
+            return null;
+        }
+
+        public void Curar(Medico medico)
+        {
+            Console.WriteLine("El medico " + medico.getNombre() + " esta curando.");
+        }
+    }
+}
