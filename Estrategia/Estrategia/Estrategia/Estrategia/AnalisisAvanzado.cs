067b4289ae3e2024b8a692800bc151287f3a083b
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-03 21:39:57 -0300

    Add files via upload

---
+++ Estrategia/Estrategia/Estrategia/Estrategia/AnalisisAvanzado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrategia.Estrategia
+{
+    public abstract class AnalisisAvanzado : IEstrategia
+    {
+        public void Analizar()
+        {
+            Iniciar();
+            AnalizarMemoria();
+            AnalizarKeyLoggers();
+            AnalizarRootKits();
+            DescomprimirZip();
+            Detener();
+        }
+
+        protected abstract void Iniciar();
+
+        protected abstract void AnalizarMemoria();
+
+        protected abstract void AnalizarKeyLoggers();
+
+        protected abstract void AnalizarRootKits();
+
+        protected abstract void DescomprimirZip();
+
+        protected abstract void Detener();
+    }
+}
