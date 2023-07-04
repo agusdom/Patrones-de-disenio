5820348bc94539f20d06fc2b5a1a5e2f40aa6c51
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-03 22:50:51 -0300

    Add files via upload

---
+++ Command/Patrón_de_diseño_command_Unity/Installer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Installer : MonoBehaviour{
+
+ [SerializeField] private Menu1 _menu1;
+ [SerializeField] private Menu2 _menu2;
+
+ [SerializeField] private CanvasGroup _canvasGroup1;
+ [SerializeField] private CanvasGroup _canvasGroup2;
+  private void Awake(){
+  var menu1Commands = new List<ICommand>{
+  new CanvasFadeCommand(_canvasGroup1,0,0.5f);
+  new CanvasFadeCommand(_canvasGroup2,1,0.5f);
+  }
+  _menu1.Configure(menu1Commands);
+ }
+}
