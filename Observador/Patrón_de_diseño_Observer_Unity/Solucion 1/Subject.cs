9b6c10021fb05ef02b3524231654b2dfbb4cb90d
agusdom <39839308+agusdom@users.noreply.github.com>
2023-07-08 16:44:42 -0300

    Delete Observador directory

--- Observador/Patrón_de_diseño_Observer_Unity/Solucion 1/Subject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Subject{
-  void Subscribe(Observer observer);
-  void UnSubscribe(Observer observer);
-  void Notify();
-}
